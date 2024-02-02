e526021b9aaecc3c49b0ecd0ca48aa7c7ec6c568
pedro.vieira <pedro.vieira@ferreiracosta.com.br>
2024-02-02 16:38:49 -0300

    modulo03 - Consultando e paginando

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -8,12 +8,29 @@ namespace FilmesAPI.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme) // [FromBody] vem do corpo da requisição
+    public IActionResult AdicionaFilme([FromBody] Filme filme) // [FromBody] vem do corpo da requisição
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);        
-        Console.WriteLine(filme.Duracao);
+        return CreatedAtAction(nameof(RecuperaFilmeId), new { id = filme.Id }, filme); // CreatedAtAction é um método que retorna um código de status 201 com os parametros: nome da ação, valores dos parâmetros e o objeto a ser retornado
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip, [FromQuery] int take)
+    // IEnumerable é uma interface que representa uma coleção de objetos que podem ser enumerados
+    // FromQuery é um atributo que indica que os parâmetros da ação devem ser obtidos da string de consulta da solicitação
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmeId(int id) // IActionResult é uma interface que representa o resultado de um método de ação
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound(); // NotFound é um método que retorna um código de status 404
+        return Ok(filme);
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,12 +4,16 @@ namespace FilmesAPI.Models;
 
 public class Filme
 {
-    [Required(ErrorMessage ="O título do filme é obrigatório")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
     public string Titulo { get; set; }
-    [Required(ErrorMessage ="O gênero do filme é obrigatório")]
-    [MaxLength(50, ErrorMessage ="o tamanho do gênero não pode exceder 50 caracteres")]
+
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "o tamanho do gênero não pode exceder 50 caracteres")]
     public string Genero { get; set; }
+
     [Required]
-    [Range(70, 600, ErrorMessage ="A duração deve ter no mínimo 70 e no máximo 600 minutos")]
+    [Range(70, 600, ErrorMessage = "A duração deve ter no mínimo 70 e no máximo 600 minutos")]
     public int Duracao { get; set; }
 }
